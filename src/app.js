348c6c64e73df630acb4a17fb55a95dbe85ababd
JanetGM <antaly.146@gmail.com>
2019-05-08 11:21:34 -0500

    agregando animaciones

---
+++ src/app.js
@@ -0,0 +1,10 @@
+// $(function() {
+//     $(window).scroll(function() {
+//   if ($(window).scrollTop() > 100) {
+//     $("#conocimientos").class('animated zoomIn delay-3s')
+    
+//   } else {
+//     $("#left-info").fadeOut();//.fadeIn();
+//   }
+//     });
+//   });
